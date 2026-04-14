1358cce37551564294242dd903d903be67eecdca
MaciOSLabAir29 <MaciOSLabAir@iOSLab-Air-29.local>
2026-04-14 13:49:16 -0600

    camara

--- SkinGuard/views/ImagePicker.swift
+++ SkinGuard/views/ImagePicker.swift
@@ -3,33 +3,37 @@ import UIKit
 
 struct ImagePicker: UIViewControllerRepresentable {
     @Binding var imagenSeleccionada: UIImage?
-    @Environment(\.presentationMode) var presentationMode
-    var sourceType: UIImagePickerController.SourceType = .camera // o .photoLibrary
-    
-    func makeUIViewController(context: Context) -> UIImagePickerController {
+    var sourceType: UIImagePickerController.SourceType
+    @Environment(\.presentationMode) private var presentationMode
+
+    func makeUIViewController(context: UIViewControllerRepresentableContext<ImagePicker>) -> UIImagePickerController {
         let picker = UIImagePickerController()
-        picker.delegate = context.coordinator
         picker.sourceType = sourceType
+        picker.delegate = context.coordinator
         return picker
     }
-    
-    func updateUIViewController(_ uiViewController: UIImagePickerController, context: Context) {}
-    
+
+    func updateUIViewController(_ uiViewController: UIImagePickerController, context: UIViewControllerRepresentableContext<ImagePicker>) {}
+
     func makeCoordinator() -> Coordinator {
         Coordinator(self)
     }
-    
+
     class Coordinator: NSObject, UINavigationControllerDelegate, UIImagePickerControllerDelegate {
-        let parent: ImagePicker
-        
+        var parent: ImagePicker
+
         init(_ parent: ImagePicker) {
             self.parent = parent
         }
-        
+
         func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
             if let imagen = info[.originalImage] as? UIImage {
                 parent.imagenSeleccionada = imagen
             }
+            parent.presentationMode.wrappedValue.dismiss()
+        }
+
+        func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
             parent.presentationMode.wrappedValue.dismiss()
         }
     }
